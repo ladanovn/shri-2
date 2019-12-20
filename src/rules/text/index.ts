0531ddce1c33c11b2a09d55c05d01e724af06f05
Ladanov Nikolay <ladanov_n@mail.ru>
2019-12-20 19:58:08 +0300

    fixed text linter

--- src/rules/text/index.ts
+++ src/rules/text/index.ts
@@ -1,7 +1,16 @@
-import { IBlock, IBlockObject, IError } from "../../interfaces";
+import { IBlock, IBlockObject, IError, ILocation } from "../../interfaces";
 
 function textLinter(block: IBlock): IError[] {
-    let prevBlock: string = "";
+    let isExistH1: boolean = false;
+    const prevBlock: {
+        value: string,
+        level: number,
+        location: ILocation,
+    } = {
+        value: "",
+        level: 0,
+        location: null,
+    };
 
     return ((): IError[] => {
         const ruleErrors: IError[] = [];
@@ -9,41 +18,53 @@ function textLinter(block: IBlock): IError[] {
 
         if (blockObject.mods) {
             if (blockObject.mods.type) {
-                if (prevBlock) {
-                    switch (blockObject.mods.type) {
-                        case "h1":
-                            ruleErrors.push({
-                                code: "TEXT.SEVERAL_H1",
-                                error: "...",
-                                location: block.location,
-                            });
-                            prevBlock = "h1";
-                            break;
 
-                        case "h2":
-                            if (prevBlock === "h1") {
-                                ruleErrors.push({
-                                    code: "TEXT.INVALID_H2_POSITION",
-                                    error: "...",
-                                    location: block.location,
-                                });
-                            }
-                            prevBlock = "h2";
-                            break;
+                if (blockObject.mods.type === "h1" && isExistH1) {
+                    ruleErrors.push({
+                        code: "TEXT.SEVERAL_H1",
+                        error: "...",
+                        location: block.location,
+                    });
+                }
 
-                        case "h3":
-                            if (prevBlock === "h2") {
-                                ruleErrors.push({
-                                    code: "TEXT.INVALID_H3_POSITION",
-                                    error: "...",
-                                    location: block.location,
-                                });
-                            }
-                            prevBlock = "h3";
-                            break;
+                if (prevBlock.value) {
+                    if (block.nestingLevel > prevBlock.level) {
+                        switch (blockObject.mods.type) {
+                            case "h1":
+                                isExistH1 = true;
+                                prevBlock.value = "h1";
+                                break;
+
+                            case "h2":
+                                if (prevBlock.value === "h1") {
+                                    ruleErrors.push({
+                                        code: "TEXT.INVALID_H2_POSITION",
+                                        error: "...",
+                                        location: prevBlock.location,
+                                    });
+                                }
+                                prevBlock.value = "h2";
+                                break;
+
+                            case "h3":
+                                if (prevBlock.value === "h2") {
+                                    ruleErrors.push({
+                                        code: "TEXT.INVALID_H3_POSITION",
+                                        error: "...",
+                                        location: prevBlock.location,
+                                    });
+                                }
+                                prevBlock.value = "h3";
+                                break;
+                        }
+                    } else {
+                        prevBlock.value = blockObject.block;
+                        prevBlock.level = block.nestingLevel;
                     }
                 } else {
-                    prevBlock = blockObject.block;
+                    prevBlock.value = blockObject.block;
+                    prevBlock.level = block.nestingLevel;
+                    prevBlock.location = block.location;
                 }
             }
         }
